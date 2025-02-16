79739f4eb96a15d3343d3f8aa0156bcaac6edbb4
iakshaytayade <akshaytayade525@gmail.com>
2025-02-16 06:39:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ auth_channel = environ.get('AUTH_CHANNEL', '0') # give your force subscribe chan
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 
 # This Channel Is For When User Request Any File Name With command or hashtag like - /request or #request
-reqst_channel = environ.get('REQST_CHANNEL', '0')
+reqst_channel = environ.get('REQST_CHANNEL', '-1002451064573')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 
 # This Channel Is For Index Request 
